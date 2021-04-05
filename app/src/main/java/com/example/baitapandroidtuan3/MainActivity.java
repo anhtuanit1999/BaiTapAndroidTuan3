59ac7a616d151c8a3eacb79793d8fc32cb5fe915
AnhTuanIT <ngocdungbb1@gmail.com>
2021-04-05 07:23:18 +0700

    done con loi

--- app/src/main/java/com/example/baitapandroidtuan3/MainActivity.java
+++ app/src/main/java/com/example/baitapandroidtuan3/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(MainActivity.this, ChonMuaActivity.class);
-        startActivityForResult(intent, reqCode);
+//        startActivityForResult(intent, reqCode);
+        startActivity(intent);
     }
 }
